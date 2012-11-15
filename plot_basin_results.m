cf2a12d2252d8d984ffd7799f2e1c3db978394b0
Paul Booth <thephantompaulbooth@gmail.com>
2012-11-14 20:50:09 -0500

    Added different phi_0 values to find basins

--- plot_basin_results.m
+++ plot_basin_results.m
@@ -5,19 +5,21 @@ function plot_basin_results (filename)
     fallen_forwards = 2;
 
     load (filename);
-    [I_fwd, J_fwd] = find (FAILURE_MODE == (fallen_forwards  * ones(size(FAILURE_MODE))) );
-    [I_bwd, J_bwd] = find (FAILURE_MODE == (fallen_backwards * ones(size(FAILURE_MODE))) );
+    for b=1:size(PHI_0S, 3)
+        [I_fwd, J_fwd] = find (FAILURE_MODE(:,:,b) == (fallen_forwards  * ones(size(FAILURE_MODE(:,:,b)))) );
+        [I_bwd, J_bwd] = find (FAILURE_MODE(:,:,b) == (fallen_backwards * ones(size(FAILURE_MODE(:,:,b)))) );
 
-    figure();
-    title ('Number of Bounces Completed');
-    xlabel ('l-dot_0');
-    ylabel ('\phi-dot_0');
+        figure();
+        title (sprintf('Number of Bounces Completed for phi=%.2f', PHI_0S(1,1,b) ));
+        xlabel ('l-dot_0');
+        ylabel ('\phi-dot_0');
 
-    hold on;
-    pcolor (L_DOT_0S, PHI_DOT_0S, NUM_BOUNCES_COMPLETED);
-    plot (L_DOT_0S(1, J_fwd), PHI_DOT_0S(I_fwd, 1), 'wx', 'MarkerSize', 5, 'MarkerFaceColor', 'white');
-    plot (L_DOT_0S(1, J_bwd), PHI_DOT_0S(I_bwd, 1), 'wo', 'MarkerSize', 5, 'MarkerFaceColor', 'white');
-    shading interp;
-    colorbar;
-    hold off;
+        hold on;
+        pcolor (L_DOT_0S(:,:,b), PHI_DOT_0S(:,:,b), NUM_BOUNCES_COMPLETED(:,:,b));
+        plot (L_DOT_0S(1, J_fwd, b), PHI_DOT_0S(I_fwd, 1, b), 'wx', 'MarkerSize', 5, 'MarkerFaceColor', 'white');
+        plot (L_DOT_0S(1, J_bwd, b), PHI_DOT_0S(I_bwd, 1, b), 'wo', 'MarkerSize', 5, 'MarkerFaceColor', 'white');
+        shading interp;
+        colorbar;
+        hold off;
+    end
 end
